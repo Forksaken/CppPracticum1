4b68316e81f2b4cd6e7f57d873c0df8fe1eebc5f
Forksaken <joystik917@gmail.com>
2024-02-22 16:19:12 +0300

    Initial commit

--- CLionProjects/pi-calc/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/forksaken/CLionProjects/pi-calc
-# Build directory: /home/forksaken/CLionProjects/pi-calc/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(run_tests "/home/forksaken/CLionProjects/pi-calc/cmake-build-debug/tests")
-set_tests_properties(run_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/forksaken/CLionProjects/pi-calc/CMakeLists.txt;17;add_test;/home/forksaken/CLionProjects/pi-calc/CMakeLists.txt;0;")
